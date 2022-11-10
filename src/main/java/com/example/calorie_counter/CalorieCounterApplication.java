2c4589c9932f581c4ccde19fa1f0bcddb31064d8
jypthemiracle <hophfg@gmail.com>
2022-11-10 11:17:10 +0900

    feat: project initialization

---
+++ src/main/java/com/example/calorie_counter/CalorieCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.calorie_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calorie_counter/CalorieCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calorie_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
