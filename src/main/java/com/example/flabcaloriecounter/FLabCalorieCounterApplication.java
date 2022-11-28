35123dffe418b38cc57ce6eeeb4f6b780910f32d
jypthemiracle <hophfg@gmail.com>
2022-11-28 16:16:47 +0900

    feat: init project

---
+++ src/main/java/com/example/flabcaloriecounter/FLabCalorieCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.flabcaloriecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FLabCalorieCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FLabCalorieCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flabcaloriecounter/FLabCalorieCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flabcaloriecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FLabCalorieCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
