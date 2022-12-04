963f448cc5ddd4f1a810c141567a053b12caba9d
youngreal <sostkffu2@daum.net>
2022-12-04 21:56:21 +0900

    test: 중복회원 가입 시 예외 테스트, 응답 Json값 테스트 추가

--- src/test/java/com/example/flabcaloriecounter/user/adapter/in/web/UserControllerTest.java
+++ src/test/java/com/example/flabcaloriecounter/user/adapter/in/web/UserControllerTest.java
@@ -1,8 +1,21 @@
 package com.example.flabcaloriecounter.user.adapter.in.web;
 
+import static com.example.flabcaloriecounter.exception.GlobalExceptionHandler.ARGUMENT_NOT_VALID_MSG;
+import static com.example.flabcaloriecounter.exception.GlobalExceptionHandler.HAS_DUPLICATED_ID_MSG;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.flabcaloriecounter.exception.HasDuplicatedIdException;
 import com.example.flabcaloriecounter.user.application.port.in.SignUpUseCase;
 import com.example.flabcaloriecounter.user.application.port.in.response.SignUpForm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +23,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
@@ -29,20 +36,37 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    SignUpForm rightUserForm;
+    SignUpForm wrongUserForm;
+
+    @BeforeEach
+    void setup() {
+        rightUserForm = new SignUpForm(
+                "rightUserId",
+                "올바른유저",
+                "12345678",
+                "dudwls0505@naver.com"
+        );
+
+        wrongUserForm = new SignUpForm(
+                "wrongUserId",
+                "잘못된유저",
+                "1",
+                "2"
+        );
+    }
+
     @Test
     @DisplayName("회원가입 성공")
     void signUp_success() throws Exception {
-        SignUpForm user = SignUpForm.builder()
-                .userId("asdf1234124213")
-                .name("이영진")
-                .password("asdfsa5678")
-                .email("asdf123456@naver.com")
-                .build();
-
         this.mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(this.rightUserForm)))
                 .andExpect(status().isCreated())
+                .andExpect(jsonPath("userId").value("rightUserId"))
+                .andExpect(jsonPath("name").value("올바른유저"))
+                .andExpect(jsonPath("password").value("12345678"))
+                .andExpect(jsonPath("email").value("dudwls0505@naver.com"))
                 .andDo(print());
 
         verify(this.signUpUseCase).signUp(any(SignUpForm.class));
@@ -51,19 +75,29 @@ class UserControllerTest {
     @Test
     @DisplayName("잘못된 Form 입력값으로 회원가입 실패")
     void signUp_wrong_input_fail() throws Exception {
-        SignUpForm user = SignUpForm.builder()
-                .userId("123131413")
-                .name("1")
-                .password("1")
-                .email("2")
-                .build();
-
         this.mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(this.objectMapper.writeValueAsString(this.wrongUserForm)))
                 .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message").value(ARGUMENT_NOT_VALID_MSG))
+                .andExpect(jsonPath("statusCode").value("BAD_REQUEST"))
                 .andDo(print());
 
         verify(this.signUpUseCase, never()).signUp(any(SignUpForm.class));
+    }
+
+    @Test
+    @DisplayName("중복 회원가입 시도시 AlreadyExistUserIdException 예외를 던진다.")
+    void signUp_AlreadyExistUserIdException_fail() throws Exception {
+        doThrow(HasDuplicatedIdException.class).when(this.signUpUseCase)
+                .signUp(any(SignUpForm.class));
+
+        this.mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(this.rightUserForm)))
+                .andDo(print())
+                .andExpect(jsonPath("message").value(HAS_DUPLICATED_ID_MSG))
+                .andExpect(jsonPath("statusCode").value("CONFLICT"))
+                .andExpect(status().isConflict());
     }
 }
