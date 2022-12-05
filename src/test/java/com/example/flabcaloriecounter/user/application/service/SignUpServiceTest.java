516b872179d15cfd8efe66230dce7e712ea171f6
jypthemiracle <hophfg@gmail.com>
2022-12-06 01:41:26 +0900

    test: inject TestUserSource into SignUpServiceTest

--- src/test/java/com/example/flabcaloriecounter/user/application/service/SignUpServiceTest.java
+++ src/test/java/com/example/flabcaloriecounter/user/application/service/SignUpServiceTest.java
@@ -4,13 +4,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.when;
 
+import java.util.stream.Stream;
+
 import com.example.flabcaloriecounter.exception.HasDuplicatedIdException;
 import com.example.flabcaloriecounter.user.application.port.in.response.SignUpForm;
 import com.example.flabcaloriecounter.user.application.port.out.SignUpPort;
+import com.example.flabcaloriecounter.user.domain.UserStatus;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -24,32 +32,25 @@ class SignUpServiceTest {
     @Mock
     SignUpPort signUpPort;
 
-    SignUpForm signUpForm;
-
     @BeforeEach
-    void setup() {
-        this.signUpForm = new SignUpForm(
-                "asdf123545",
-                "이영진",
-                "12345678",
-                "dudwls0505@naver.com"
-        );
-    }
+    void setup() {}
 
-    @Test
-    @DisplayName("중복된 아이디가 있으면 예외를던진다")
-    void signUp_existEmail_fail() {
-        when(this.signUpPort.hasDuplicatedId(this.signUpForm.userId())).thenReturn(true);
+    @ParameterizedTest
+    @MethodSource("TestUserSource#일반사용자_하나_정보제공자_하나")
+    @DisplayName("중복된 아이디가 있으면 예외를 던진다")
+    void signUp_existEmail_fail(SignUpForm signUpForm) {
+        when(this.signUpPort.hasDuplicatedId(signUpForm.userId())).thenReturn(true);
 
-        assertThatThrownBy(() -> this.signUpService.signUp(this.signUpForm))
+        assertThatThrownBy(() -> this.signUpService.signUp(signUpForm))
                 .isInstanceOf(HasDuplicatedIdException.class);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("TestUserSource#일반사용자_하나_정보제공자_하나")
     @DisplayName("중복된 아이디가 없으면 정상처리한다")
-    void signUp_success() {
+    void signUp_success(SignUpForm signUpForm) {
         when(this.signUpPort.hasDuplicatedId(signUpForm.userId())).thenReturn(false);
 
-        assertDoesNotThrow(() -> this.signUpService.signUp(this.signUpForm));
+        assertDoesNotThrow(() -> this.signUpService.signUp(signUpForm));
     }
 }
