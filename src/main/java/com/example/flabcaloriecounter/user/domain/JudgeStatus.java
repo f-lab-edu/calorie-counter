ecd2d1a45c6873570e32bc618258c7439a8e4129
jypthemiracle <hophfg@gmail.com>
2022-12-07 20:52:48 +0900

    feat: return initial JudgeStatus by UserStatus before SignUpForm

--- src/main/java/com/example/flabcaloriecounter/user/domain/JudgeStatus.java
+++ src/main/java/com/example/flabcaloriecounter/user/domain/JudgeStatus.java
@@ -1,5 +1,11 @@
 package com.example.flabcaloriecounter.user.domain;
 
 public enum JudgeStatus {
-    PENDING, PASSED, REJECTED, NOT_ASSIGNED
+    PENDING, PASSED, REJECTED, NOT_ASSIGNED;
+    public static JudgeStatus getInitialJudgeStatusByUserStatus(UserStatus userStatus) {
+        if (userStatus.isProvider()) {
+            return JudgeStatus.PENDING;
+        }
+        return JudgeStatus.NOT_ASSIGNED;
+    }
 }
