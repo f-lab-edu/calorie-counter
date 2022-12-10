703c283e415ff469eff6eab22513ceae061b814e
jypthemiracle <hophfg@gmail.com>
2022-12-10 13:48:51 +0900

    refactor: add valueOf method in UserStatus

--- src/main/java/com/example/flabcaloriecounter/user/domain/UserStatus.java
+++ src/main/java/com/example/flabcaloriecounter/user/domain/UserStatus.java
@@ -1,5 +1,25 @@
 package com.example.flabcaloriecounter.user.domain;
 
 public enum UserStatus {
-    WITHDRAWAL, NOT_WITHDRAWAL
+    WITHDRAWAL(0),
+    NOT_WITHDRAWAL(1);
+
+    private int statusCode;
+
+    UserStatus(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public static UserStatus valueOf(int value) {
+        for (UserStatus userStatus : values()) {
+            if (userStatus.getStatusCode() == value) {
+                return userStatus;
+            }
+        }
+        throw new IllegalArgumentException("Invalid UserStatus code: " + value);
+    }
 }
