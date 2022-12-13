c4241663b2c36a8cdf35996af0be5929d07f676e
jypthemiracle <hophfg@gmail.com>
2022-12-13 11:48:18 +0900

    refactor: change for-loop to stream API - extract Suppliers to different method

--- src/main/java/com/example/flabcaloriecounter/user/domain/UserStatus.java
+++ src/main/java/com/example/flabcaloriecounter/user/domain/UserStatus.java
@@ -1,5 +1,9 @@
 package com.example.flabcaloriecounter.user.domain;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public enum UserStatus {
     WITHDRAWAL("탈퇴상태"),
     NOT_WITHDRAWAL("정상상태");
@@ -14,12 +18,22 @@ public enum UserStatus {
         return this.statusMessage;
     }
 
+    public boolean checkStatusMessage(String statusMessage) {
+        return this.statusMessage.equals(statusMessage);
+    }
+
     public static UserStatus findByMessage(String statusMessage) {
-        for (UserStatus userStatus : UserStatus.values()) {
-            if (userStatus.getStatusMessage().equals(statusMessage)) {
-                return userStatus;
-            }
-        }
-        throw new IllegalArgumentException("해당하는 상태가 없습니다.");
+        return Arrays.stream(UserStatus.values())
+                .filter(getUserStatusPredicate(statusMessage))
+                .findFirst()
+                .orElseThrow(NonStatusFoundErrorSupplier());
+    }
+
+    private static Supplier<IllegalArgumentException> NonStatusFoundErrorSupplier() {
+        return () -> new IllegalArgumentException("해당하는 상태가 없습니다.");
+    }
+
+    private static Predicate<UserStatus> getUserStatusPredicate(String statusMessage) {
+        return userStatus -> userStatus.checkStatusMessage(statusMessage);
     }
 }
