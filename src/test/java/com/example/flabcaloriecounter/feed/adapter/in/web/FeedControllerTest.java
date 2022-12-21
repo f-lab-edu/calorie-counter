85deba86a3a2b51a158beb606000f3064fd31bc7
youngreal <sostkffu2@daum.net>
2022-12-22 02:45:50 +0900

    test: 피드등록 관련 테스트

--- src/test/java/com/example/flabcaloriecounter/feed/adapter/in/web/FeedControllerTest.java
+++ src/test/java/com/example/flabcaloriecounter/feed/adapter/in/web/FeedControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.flabcaloriecounter.feed.adapter.in.web;
 
+import static com.example.flabcaloriecounter.exception.GlobalExceptionHandler.EMPTY_FEED_MSG;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +12,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.flabcaloriecounter.feed.application.port.in.response.FeedDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -26,62 +29,95 @@ class FeedControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	FeedDto rightFeedDto;
-	FeedDto wrongFeedDto;
+	MockMultipartFile image1;
+	MockMultipartFile contents;
+	MockMultipartFile nullContents;
+	MockMultipartFile image2;
 
-	@BeforeEach
-	void setup() {
-		this.rightFeedDto = new FeedDto(
-			"닭가슴살을 먹었다"
-		);
-
-		this.wrongFeedDto = new FeedDto(
-			""
-		);
-
+	record FeedTestDto(String contents) {
 	}
 
-	@Test
-	@DisplayName("피드 작성 성공 : 이미지, 내용 둘다 있는경우")
-	void feed_photo_test() throws Exception {
-		MockMultipartFile image1 = new MockMultipartFile(
-			"photos",
+	@BeforeEach
+	void setup() throws JsonProcessingException {
+		this.image1 = new MockMultipartFile(
+			"feedDto",
 			"photos",
 			"image/jpeg",
 			"photos".getBytes()
 		);
-		MockMultipartFile image2 = new MockMultipartFile(
-			"photos",
+
+		this.image2 = new MockMultipartFile(
+			"feedDto",
 			"photos2",
 			"image/jpeg",
 			"photos2".getBytes()
 		);
 
+		this.contents = new MockMultipartFile(
+			"feedDto",
+			"feedDto",
+			"application/json",
+			objectMapper.writeValueAsString(new FeedTestDto("닭가슴살을 먹었다")).getBytes()
+		);
+
+		this.nullContents = new MockMultipartFile(
+			"feedDto",
+			"feedDto",
+			"application/json",
+			objectMapper.writeValueAsString(new FeedTestDto("")).getBytes()
+		);
+	}
+
+	//todo 로그인한 유저여야한다
+	@Test
+	@DisplayName("피드 작성 성공 : 이미지, 내용 둘다 있는경우")
+	void feed_write_test() throws Exception {
 		this.mockMvc.perform(multipart("/feeds")
-				.file(image1)
-				.file(image2)
-				.param("content", this.rightFeedDto.contents())
-			)
+				.file(this.contents)
+				.file(this.image1)
+				.file(this.image2)
+				.contentType(MediaType.MULTIPART_FORM_DATA))
 			.andDo(print())
 			.andExpect(status().isCreated());
 	}
 
-	//todo 피드 작성 성공 : 이미지나 내용 둘중 하나만 있는경우
+	//todo 로그인한 유저여야한다
+	@Test
+	@DisplayName("피드 작성 성공 : 이미지만 있는경우")
+	void feed_write_test2() throws Exception {
+		this.mockMvc.perform(multipart("/feeds")
+				.file(this.nullContents)
+				.file(this.image1)
+				.file(this.image2)
+				.contentType(MediaType.MULTIPART_FORM_DATA))
+			.andDo(print())
+			.andExpect(status().isCreated());
+	}
 
-	//todo 피드 작성 실패 : 내용, 이미지 모두 비어있음
+	//todo 로그인한 유저여야한다
+	@Test
+	@DisplayName("피드 작성 성공 : 내용만 있는경우")
+	void feed_write_test3() throws Exception {
+		this.mockMvc.perform(multipart("/feeds")
+				.file(contents)
+				.contentType(MediaType.MULTIPART_FORM_DATA))
+			.andDo(print())
+			.andExpect(status().isCreated());
+	}
 
-	// @Test
-	// @DisplayName("피드 작성 실패 : 내용, 이미지 모두 비어있음")
-	// void feed_write_fail_emptyContent() throws Exception {
-	// 	this.mockMvc.perform(post("/feeds")
-	// 			.content(this.objectMapper.writeValueAsString(this.wrongFeedDto)))
-	// 		.andExpect(status().isBadRequest())
-	// 		.andExpect(jsonPath("message").value(ARGUMENT_NOT_VALID_MSG))
-	// 		.andExpect(jsonPath("statusCode").value("BAD_REQUEST"))
-	// 		.andDo(print());
-	// }
+	//todo 로그인한 유저여야한다
+	@Test
+	@DisplayName("피드 작성 실패 : 내용, 이미지 모두 비어있는경우")
+	void feed_write_test4() throws Exception {
+		this.mockMvc.perform(multipart("/feeds")
+				.contentType(MediaType.MULTIPART_FORM_DATA))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("message").value(EMPTY_FEED_MSG))
+			.andExpect(jsonPath("statusCode").value("BAD_REQUEST"))
+			.andDo(print());
+	}
 
-	//todo 로그인 추가되면 마저 작성
+	//todo 로그인 추가되면 마저 작성 : 로그인 하지 않은경우 피드 작성 실패
 
 	// @Test
 	// @DisplayName("로그인 하지 않은경우 피드 작성 실패")
--- src/test/java/com/example/flabcaloriecounter/feed/application/service/FeedServiceTest.java
+++ src/test/java/com/example/flabcaloriecounter/feed/application/service/FeedServiceTest.java
@@ -1,16 +1,23 @@
 package com.example.flabcaloriecounter.feed.application.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.flabcaloriecounter.feed.application.port.in.response.FeedDto;
+import com.example.flabcaloriecounter.feed.application.port.in.dto.FeedDto;
+import com.example.flabcaloriecounter.feed.application.port.in.dto.ImageUploadDto;
 import com.example.flabcaloriecounter.feed.application.port.out.FeedPort;
+import com.example.flabcaloriecounter.feed.domain.Feed;
 import com.example.flabcaloriecounter.user.adapter.out.persistence.UserRepository;
 import com.example.flabcaloriecounter.user.application.port.in.response.SignUpForm;
 import com.example.flabcaloriecounter.user.domain.JudgeStatus;
@@ -18,26 +25,28 @@ import com.example.flabcaloriecounter.user.domain.UserType;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class FeedServiceTest {
 
 	@Autowired
-	FeedService feedService;
+	private FeedService feedService;
 
 	@Autowired
-	FeedPort feedPort;
+	private FeedPort feedPort;
 
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
 
-	FeedDto rightFeed;
-	FeedDto wrongFeed;
-
-	SignUpForm signUpForm;
+	private FeedDto contentsFeed;
+	private SignUpForm signUpForm;
+	private List<ImageUploadDto> imageInfos;
+	private List<ImageUploadDto> onlyImageInfos;
 
 	@BeforeEach
 	void setup() {
-		this.rightFeed = new FeedDto(
-			"피드의 제목"
+		this.contentsFeed = new FeedDto(
+			"닭가슴살을 먹었다",
+			null
 		);
 
 		this.signUpForm = new SignUpForm(
@@ -51,18 +60,53 @@ class FeedServiceTest {
 		);
 	}
 
+	//todo 로그인 유무 check
 	@Test
 	@DisplayName("피드 작성 성공 : contents만 있음 ")
 	void feed_write_success() {
 		this.userRepository.signUp(this.signUpForm);
 
-		assertDoesNotThrow(() -> this.feedService.write(this.rightFeed, null));
+		assertDoesNotThrow(() -> this.feedService.write(this.contentsFeed, 1));
+
+		assertThat(this.feedService.findByFeedId(1)).isEqualTo(
+			Optional.of(new Feed(1, this.contentsFeed.contents(), 1)));
 	}
-	//
+
+	//todo 로그인 유무 check
+	@Test
+	@DisplayName("피드 작성 성공 : contents, Image 둘다 있음")
+	void feed_write_success2() {
+		this.userRepository.signUp(this.signUpForm);
+
+		this.imageInfos = List.of(
+			new ImageUploadDto("image1.png", "local/2022/1221/user12q3wqeqwe.png", 1),
+			new ImageUploadDto("image2.png", "local/2022/1221/user211231q3wqeqwe.png", 2));
+
+		assertDoesNotThrow(() -> this.feedPort.insertImage(this.imageInfos));
+	}
+
+	//todo 로그인 유무 check
+	@Test
+	@DisplayName("피드 작성 성공 : Image만 있음")
+	void feed_write_success3() {
+		this.userRepository.signUp(this.signUpForm);
+
+		this.onlyImageInfos = List.of(
+			new ImageUploadDto("image3.png", "local/2022/1221/user12q3wqeqwe.png", 1),
+			new ImageUploadDto("image4.png", "local/2022/1221/user211231q3wqeqwe.png", 2));
+
+		assertDoesNotThrow(() -> this.feedPort.insertImage(this.onlyImageInfos));
+	}
+
+	//todo 피드 작성 실패 : 유저가 로그인하지않음
+
+	//todo 피드 작성 실패 : 유저가 존재하지않음
+
 	//    @Test
 	//    @DisplayName("피드 작성 실패: 유저가 존재하지않는다")
 	//    void feed_write_fail_notExistUser() {
 	//        assertThatThrownBy(() -> this.feedService.write(this.rightFeed))
 	//                .isInstanceOf(UserNotFoundException.class);
 	//    }
+
 }
