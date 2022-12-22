435029d5e25a8b310ca87c61a1dafced728b9b4e
jypthemiracle <theeconomist@kakao.com>
2022-12-22 11:32:27 +0900

    feat: CustomFilter 생성 후 RouteLocator에 담아 빈 등록

--- src/main/java/com/example/flabcaloriecountergateway/config/FilterConfig.java
+++ src/main/java/com/example/flabcaloriecountergateway/config/FilterConfig.java
@@ -1,5 +1,9 @@
 package com.example.flabcaloriecountergateway.config;
 
+import com.example.flabcaloriecountergateway.filter.CustomFilter;
+import com.example.flabcaloriecountergateway.filter.CustomFilterConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,16 +13,18 @@ import java.util.UUID;
 
 @Configuration
 public class FilterConfig {
+
+    @Autowired
+    private CustomFilter customFilter;
+
     @Bean
+    // Bean 등록
     public RouteLocator gatewayRoutes(RouteLocatorBuilder rlbuilder) {
         String sessionId = getSessionId();
+        GatewayFilter appliedFilter = customFilter.apply(new CustomFilterConfig());
         return rlbuilder.routes()
                 .route(r -> r.path("/v1/**")
-                        .filters(f -> f.addRequestHeader("x-session-id", sessionId)
-                                .addResponseHeader("x-session-id", sessionId))
-                        .uri("http://localhost:8080"))
-                .route(r -> r.path("/users/**")
-                        .filters(f -> f.addRequestHeader("x-session-id", sessionId)
+                        .filters(f -> f.filter(appliedFilter).addRequestHeader("x-session-id", sessionId)
                                 .addResponseHeader("x-session-id", sessionId))
                         .uri("http://localhost:8080"))
                 .build();
---
+++ src/main/java/com/example/flabcaloriecountergateway/filter/CustomFilterConfig.java
@@ -0,0 +1,5 @@
+package com.example.flabcaloriecountergateway.filter;
+
+public class CustomFilterConfig {
+    // Put the configuration properties
+}
