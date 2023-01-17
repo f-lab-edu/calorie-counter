aac5da0d36ee6666a2c15528a364045258eeb91f
youngreal <sostkffu2@daum.net>
2023-01-17 17:47:17 +0900

    feat: 레디스에 유저 캐시

--- src/main/java/com/example/flabcaloriecounter/user/domain/User.java
+++ src/main/java/com/example/flabcaloriecounter/user/domain/User.java
@@ -2,6 +2,11 @@ package com.example.flabcaloriecounter.user.domain;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 public record User(
 	long id,
 	String userId,
@@ -10,6 +15,8 @@ public record User(
 	String email,
 	Double weight,
 	String withdrawalReason,
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	LocalDateTime joinDate,
 	UserStatus userStatus,
 	UserType userType,
