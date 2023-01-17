c4f665c9627e626e1bbf277c8d8fe2293991b214
youngreal <sostkffu2@daum.net>
2023-01-17 17:56:27 +0900

    chore: 중복되는 yml부분 삭제, 여백 자동수정

--- src/main/java/com/example/flabcaloriecounter/user/adapter/out/persistence/UserMapper.java
+++ src/main/java/com/example/flabcaloriecounter/user/adapter/out/persistence/UserMapper.java
@@ -1,17 +1,20 @@
 package com.example.flabcaloriecounter.user.adapter.out.persistence;
 
+import java.util.Optional;
+
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.flabcaloriecounter.user.application.port.in.response.SignUpForm;
 import com.example.flabcaloriecounter.user.domain.User;
-import java.util.Optional;
-import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
 
-    void signUp(final SignUpForm signUpForm);
+	void signUp(final SignUpForm signUpForm);
 
-    boolean hasDuplicatedId(final String userId);
+	boolean hasDuplicatedId(final String userId);
 
-    Optional<User> findByUserId(final String userId);
+	Optional<User> findByUserId(final String userId);
+
 	String getPassword(final String userId);
 }
--- src/main/java/com/example/flabcaloriecounter/user/application/port/in/SignUpUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.example.flabcaloriecounter.user.application.port.in;
-
-import com.example.flabcaloriecounter.user.application.port.in.response.SignUpForm;
-
-public interface SignUpUseCase {
-
-    void signUp(final SignUpForm signUpForm);
-}
